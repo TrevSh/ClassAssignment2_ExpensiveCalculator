f14b37679c8e9245f5850f3ce47386d0c06e8c2d
TrevSh <trevonshelby@outlook.com>
2023-07-21 15:09:23 -0500

    Added text from book Pg.16.

--- ClassAssignment2_ExpensiveCalculator/ClassAssignment2_ExpensiveCalculator.cpp
+++ ClassAssignment2_ExpensiveCalculator/ClassAssignment2_ExpensiveCalculator.cpp
@@ -3,6 +3,24 @@
 
 #include <iostream>
 
+/*
+ https://pisaucer.github.io/book-c-plus-plus/Beginning_Cpp_Through_Game_Programming.pdf
+ 
+    Understanding Order of Operations
+Just as in algebra, arithmetic expressions in Cþþ are evaluated from left to right.
+But some operators have a higher precedence than others and are evaluated first,
+regardless of position. Multiplication, division, and modulus have equal precedence, which is higher than the precedence level that addition and subtraction
+share.
+The next line of code provides an example to help drive this home. Because
+multiplication has higher precedence than addition, you calculate the results of
+the multiplication first. So the expression 7 þ 3*5 is equivalent to 7 þ 15, which
+evaluates to 22.
+If you want an operation with lower precedence to occur first, you can use
+parentheses, which have higher precedence than any arithmetic operator. So in
+the next statement, the expression (7 þ 3) * 5 is equivalent to 10 * 5, which
+evaluates to 50.
+*/
+
 int main()
 {
     std::cout << "7 + 3 = " << 7 + 3 << '\n';
