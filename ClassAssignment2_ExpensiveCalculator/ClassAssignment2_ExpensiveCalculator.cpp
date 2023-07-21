0dd6c9b51a221ff4b664e743ed734e43f60f4ff2
TrevSh <trevonshelby@outlook.com>
2023-07-21 14:52:37 -0500

    Add project files.

---
+++ ClassAssignment2_ExpensiveCalculator/ClassAssignment2_ExpensiveCalculator.cpp
@@ -0,0 +1,21 @@
+// ClassAssignment2_ExpensiveCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "7 + 3 = " << 7 + 3 << '\n';
+    std::cout << "7 - 3 = " << 7 - 3 << '\n';
+    std::cout << "7 * 3 = " << 7 * 3 << '\n';
+
+    std::cout << "7 / 3 = " << 7 / 3 << '\n';
+    std::cout << "7.0 / 3.0 = " << 7.0 / 3.0 << '\n';
+
+    std::cout << "7 % 3 = " << 7 % 3 << '\n';
+
+    std::cout << "7 + 3 * 5 = " << 7 + 3 * 5 << '\n';
+    std::cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << '\n';
+
+    return 0;
+}
